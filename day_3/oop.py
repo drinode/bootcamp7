ef6a80990485d0a90c809b1613e7805c6443812c
Emmah Kimari <emalindahkimari@gmail.com>
2016-04-27 16:41:55 +0300

    Assignment

--- day_3/oop.py
+++ day_3/oop.py
@@ -1,26 +1,18 @@
+from person import Person
+from kenya import Kenyan
+#ASIDE
 #PEP8
 #
 #Instance vs class variables
-class Person:
-	#class variables
-	people_count = 0
-	def __init__(self, name, age):
-		#instance variiables
-		self.name = name
-		self.age = age
-		Person.people_count += 1
-		
-	def __repr__(self):
-		return "<object: {}, {}>".format(self.name, self.age)
+#class methods (advanced topic)
 
-	def say_hello(self):
-		return "Hello I'm {} and I'm {} years old".format(self.name, self.age)
 
+
+#end of class
 p = Person('Emmah',19)
 p2 = Person('Joe', 25)
 p3 = Person('Loulou',35)
 print (p.say_hello())
-
 a = [('Jane', 23),('Joe',50),('Jacob',20),('Jee',24),('Josh', 60)]
 '''
 b = []
@@ -38,3 +30,9 @@ print (p.age)
 print (p.people_count)
 print (b)
 
+
+k = Kenyan('Anita Waiguru', 30)
+k.probe(True)
+
+print ("Is {} corrupt? {}".format(k.name, k.is_corrupt()))	
+print (k.say_hello())
