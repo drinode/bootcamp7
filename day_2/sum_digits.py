a4a7ecade9e679379293adf64266f0994cd779f5
Emmah Kimari <emalindahkimari@gmail.com>
2016-04-28 16:13:27 +0300

    Day 2

--- day_2/sum_digits.py
+++ day_2/sum_digits.py
@@ -1,20 +1,20 @@
 def sum_digits(A):
 
-	'''
-	Takes a list A, and returns the sum of all the 
-	digits in the list
-	e.g. [10,30,45] should return 1+0+3+0+4+5=13
-	'''
-	
-	int_i == 0
-	str_i == 0
+    '''
+    Takes a list A, and returns the sum of all the 
+    digits in the list
+    e.g. [10,30,45] should return 1+0+3+0+4+5=13
+    '''
+    
+    int_i == 0
+    str_i == 0
   
     for i in A:
-  	   str_i = str(int_i)
-  	    for char_i in str_i:
-  	        int_i += int(char_i)
-	print int_i
+        str_i = str(int_i)
+        for char_i in str_i:
+           int_i += int(char_i)
+        print (int_i)
 
-sum_digits(10,30,45)
+sum_digits([10,30,45])
 
 
